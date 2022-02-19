02f5d5c23c852ecd9c359f5aecb7912f48381415
Jere Nuora <jere.nuora@gmail.com>
2022-02-19 20:14:34 +0200

    8.2

--- library-backend/index.js
+++ library-backend/index.js
@@ -92,13 +92,24 @@ const typeDefs = gql`
   type Query {
       bookCount: Int!
       authorCount: Int!
+      allBooks: [Book!]!
   }
+
+  type Book {
+        title: String! 
+        author: String!
+        published: String! 
+        genres: [String!]!
+      }
+    
+  
 `
 
 const resolvers = {
   Query: {
       bookCount: () => books.length,
-      authorCount: () => authors.length
+      authorCount: () => authors.length,
+      allBooks: () => books
   }
 }
 
