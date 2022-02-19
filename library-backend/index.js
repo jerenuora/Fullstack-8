0e0ac23e1060a15dd77b8f6336c17afa845573b9
Jere Nuora <jere.nuora@gmail.com>
2022-02-19 20:43:10 +0200

    8.4

--- library-backend/index.js
+++ library-backend/index.js
@@ -92,7 +92,7 @@ const typeDefs = gql`
   type Query {
       bookCount: Int!
       authorCount: Int!
-      allBooks: [Book!]!
+      allBooks(author: String): [Book!]!
       allAuthors: [Author!]!
   }
 
@@ -114,7 +114,7 @@ const resolvers = {
   Query: {
       bookCount: () => books.length,
       authorCount: () => authors.length,
-      allBooks: () => books,
+      allBooks: (root, args) => books.filter(b => b.author === args.author),
       allAuthors: () => authors
   },
   Author: {
