d30c85f4f04ef75e65ea6aa778b80bf2647805d5
Jere Nuora <jere.nuora@gmail.com>
2022-02-28 18:02:19 +0200

    8.19

--- library-frontend/src/components/Books.js
+++ library-frontend/src/components/Books.js
@@ -1,22 +1,38 @@
-import { ALL_BOOKS } from '../queries' 
+import { ALL_BOOKS } from '../queries'
 import { useQuery } from '@apollo/client'
-
+import { useState } from 'react'
 const Books = ({ show }) => {
   const books = useQuery(ALL_BOOKS)
+  const [genreToShow, setGenresToShow] = useState(null)
 
   if (!show) {
     return null
   }
-  if (books.loading)  {
+  if (books.loading) {
     return <div>loading...</div>
   }
 
   const booksToShow = books.data.allBooks
+  const genres = new Set(
+    booksToShow.reduce((list, book) => list.concat(book.genres), [])
+  )
+  const genreButtons = [...genres].map((genre) => {
+    return <button key={genre} onClick={() => setGenresToShow(genre)}>{genre}</button>
+  })
 
+  console.log(genreToShow)
+  console.log(booksToShow)
+  let booksByGenre = booksToShow
+
+  if (genreToShow) {
+    booksByGenre = booksToShow.filter((book) =>
+      book.genres.includes(genreToShow) ? book : null
+    )
+  }
   return (
     <div>
-      <h2>books</h2>
-
+      <h2>books by genre {genreToShow}</h2>
+      {genreButtons}
       <table>
         <tbody>
           <tr>
@@ -24,7 +40,7 @@ const Books = ({ show }) => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {booksToShow.map((a) => (
+          {booksByGenre.map((a) => (
             <tr key={a.id}>
               <td>{a.title}</td>
               <td>{a.author.name}</td>
